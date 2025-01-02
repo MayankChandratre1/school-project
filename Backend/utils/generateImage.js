9cc7b58c6d124770509b98e401007d6dcbdc1ebc
Mayank <mayankmchandratre@gmail.com>
2025-01-02 23:48:55 +0530

    update 02012025 2335

--- Backend/utils/generateImage.js
+++ Backend/utils/generateImage.js
@@ -1,7 +1,12 @@
 import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { createCanvas, loadImage } from 'canvas';
+import { createCanvas, loadImage, registerFont } from 'canvas';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Register the font
+registerFont(path.join(__dirname, './fonts/ARIAL.TTF'), { family: 'Arial' });
 
 export const generateCouponImage = async (
   noOfTokens,
@@ -15,56 +20,38 @@ export const generateCouponImage = async (
   parentEmail,
 ) => {
   try {
-    // Resolve the directory name
-    const __dirname = dirname(fileURLToPath(import.meta.url));
-    
-    // Load the background image
-    const backgroundImage = await loadImage("https://res.cloudinary.com/dvsl1aslo/image/upload/v1735839196/school_token_qvqoxg.png");
-    
-    // Load the school logo
-    
-    
+    const backgroundImage = await loadImage(
+      'https://res.cloudinary.com/dvsl1aslo/image/upload/v1735839196/school_token_qvqoxg.png'
+    );
     const schoolLogo = await loadImage(schoolLogoURL);
-    console.log("loaded");
-    
-    
-    // Create a canvas
-    const canvas = createCanvas(690, 400); // Adjusted height for additional text
-    const ctx = canvas.getContext('2d');
-    console.log(ctx.font);
-    
-    // Draw the background image
-    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
 
-    // Draw the school logo in the top-left corner
-    const logoWidth = 80; // Adjust size as needed
-    const logoHeight = 80;
-    ctx.drawImage(schoolLogo, 80, 80, logoWidth, logoHeight);
+    const canvas = createCanvas(690, 400);
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(schoolLogo, 80, 80, 80, 80);
 
     // Set font and style for text
-    
+    ctx.font = '20px Arial';
     ctx.fillStyle = 'black';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
 
-    // Add school name
     ctx.fillText(schoolName, canvas.width / 2, 40);
-
-    // Add dynamic text
     ctx.fillText(`Student: ${student}`, canvas.width / 2, 150);
     ctx.fillText(`No. of Tokens: ${noOfTokens}`, canvas.width / 2, 180);
     ctx.fillText(`Teacher: ${teacher} (${subject})`, canvas.width / 2, 200);
     ctx.fillText(`Teacher Email: ${teacherEmail}`, canvas.width / 2, 230);
     ctx.fillText(`Parent Email: ${parentEmail}`, canvas.width / 2, 250);
     ctx.fillText(`Date: ${date}`, canvas.width / 2, 300);
-    
-    // Convert canvas to a buffer
+
     return canvas.toBuffer();
   } catch (error) {
     console.error('Error generating coupon image:', error);
     throw error;
   }
 };
+
 
 async function testLoadImage() {
     try {
