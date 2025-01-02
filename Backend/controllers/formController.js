7daf60930bd5e518c2cd597b17351f01f74ffd74
Mayank <mayankmchandratre@gmail.com>
2025-01-02 23:13:09 +0530

    update 02012025

--- Backend/controllers/formController.js
+++ Backend/controllers/formController.js
@@ -155,15 +155,15 @@ export const submitFormTeacher = async (req, res) => {
         
 
         if(form.teacherEmail && teacher.recieveMails)
-             sentToTeacher = await sendEmail(teacher.email, 'Form Submitted', info, info, attachment)
+             await sendEmail(teacher.email, 'Form Submitted', info, info, attachment)
         if(form.studentEmail)
-             sentToStudent = await sendEmail(submittedForStudent.email, 'Form Submitted', info, info, attachment)
+              await sendEmail(submittedForStudent.email, 'Form Submitted', info, info, attachment)
         if(form.schoolAdminEmail)
-             sentToAdmin = await sendEmail(schoolAdmin.email,'Form Submitted', info, info, attachment)
+              await sendEmail(schoolAdmin.email,'Form Submitted', info, info, attachment)
         if(form.parentEmail && submittedForStudent.parentEmail && submittedForStudent.sendNotifications)
-             sentToParent1 = await sendEmail(submittedForStudent.parentEmail, 'Form Submitted', info, info, attachment)
+              await sendEmail(submittedForStudent.parentEmail, 'Form Submitted', info, info, attachment)
         if(form.parentEmail && submittedForStudent.standard && submittedForStudent.sendNotifications)
-             sentToParent2 = await sendEmail(submittedForStudent.standard, 'Form Submitted', info, info, attachment)
+              await sendEmail(submittedForStudent.standard, 'Form Submitted', info, info, attachment)
 
         }
 
